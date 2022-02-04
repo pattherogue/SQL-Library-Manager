2269050f04ca9c6791b43111975124cdacc10043
Paolo Gomez <peweegardner248@gmail.com>
2022-02-04 14:34:23 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
 
+function asyncHandler(cb) {
+  return async(req, res, next) => {
+    
+  }
+}
+
 /* GET home page. */
 router.get('/books', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
