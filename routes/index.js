edee932290f75d3626293af1e73c1d37ed84b628
Paolo Gomez <peweegardner248@gmail.com>
2022-02-17 12:11:21 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+/* import Book model from "../models" folder */
+var Book = require('../models').Books;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
