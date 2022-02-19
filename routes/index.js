4731d1f66b495cd2990eeb600206a2f3cfbcbf86
Paolo Gomez <peweegardner248@gmail.com>
2022-02-19 09:15:20 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,8 @@ router.get('/books', asyncHandler(async(req, res) => {
 router.post('/books/new', asyncHandler(async (req, res) => {
     /* store in variable */
   const books = await Book.findAll();
+    /* log out books variable */
+  console.log(books);
 }));
 
 
