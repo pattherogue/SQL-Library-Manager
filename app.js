48ac4ac77c3fb7d2f231c970906858e83ff10599
Paolo Gomez <peweegardner248@gmail.com>
2022-02-27 13:17:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -69,13 +69,9 @@ app.use((req, res) => {
 /* log status and message to console */
 /* render "error" template */ 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (err.status === 404) {
+    res.render("page-not-found", { error });
+  }
 });
 
 module.exports = app;
