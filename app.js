4f3e8a468250d850923ec209099c5487d5470f1e
Paolo Gomez <peweegardner248@gmail.com>
2022-03-02 10:56:41 -0800

    update

--- app.js
+++ app.js
@@ -58,10 +58,6 @@ app.use((req, res, next) => {
 
 const port = 3000
 
-app.listen(port, (err) => {
-  if (err) console.log(err);
-  console.log('Server is listening on port' + port)
-});
 
 // catch 404 and forward to error handler
 app.use((req, res) => {
