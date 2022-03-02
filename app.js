fe65933d8bad37bb40d688757ad96a73ed4660a5
Paolo Gomez <peweegardner248@gmail.com>
2022-03-02 09:20:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -49,8 +49,17 @@ app.use('/users', usersRouter);
     
 }) ();
 
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  if (!req.user) return next(
+    createError(404, 'Login Required'));
+    next();
+});
+
+const port = 3000
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  console.log('Server is listening on port' + port)
 });
 
 // catch 404 and forward to error handler
