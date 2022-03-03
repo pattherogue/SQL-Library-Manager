081e3064ea4d412f93039796162471d25ac0641a
Paolo Gomez <peweegardner248@gmail.com>
2022-03-03 11:29:30 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ function asyncHandler(cb) {
   return async(req, res, next) => {
     try {
       await cb (req, res, next);
-    } catch (error) {
-      next(error);
+    } catch (err) {
+      next(err);
     }
   }
 }
